d0e25dc2f7ba31e9249ceb50f2bfac7c3059f8dc
Sandr <sandrajakobsson81@gmail.com>
2026-03-06 18:18:05 +0100

    updating navbar and pages

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Vovvar site startad");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
